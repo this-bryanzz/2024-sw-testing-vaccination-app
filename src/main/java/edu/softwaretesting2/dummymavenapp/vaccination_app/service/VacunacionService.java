f07cc88af571e94db0ef5e2635da534f46e8595e
BrayanC17 <bcastrovarela@ucompensar.edu.co>
2024-10-20 23:05:55 -0500

    Update VacunacionService.java
    Actualización de la función de comprobación de edad para la priorización

--- src/main/java/edu/softwaretesting2/dummymavenapp/vaccination_app/service/VacunacionService.java
+++ src/main/java/edu/softwaretesting2/dummymavenapp/vaccination_app/service/VacunacionService.java
@@ -5,9 +5,13 @@ import edu.softwaretesting2.dummymavenapp.vaccination_app.model.Paciente;
 import java.time.LocalDate;
 
 public class VacunacionService {
-    public void validarLaEdadDelPaciente(Paciente p) {
+    public boolean validarLaEdadDelPaciente(Paciente p) {
+        
         LocalDate fechaLímite = LocalDate.now().minusYears(60);
+        boolean Prioritario = p.getFechaDeNacimiento().isBefore(fechaLímite);
 
-        p.setPrioritario(p.getFechaDeNacimiento().isBefore(fechaLímite));
+        p.setPrioritario(Prioritario);
+
+        return Prioritario;
     }
 }
