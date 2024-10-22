46d06e2e2bcfd1e93904e7847c73dbc4c6fa6334
John Munoz <jmunoztopa@gmail.com>
2024-10-21 20:09:00 -0500

    test_titan 2024/10/21
    * Solucion de error en VacunacionServiceTest.java
    * Integrantes:
    -Edwin Luque
    -John Muñoz
    -Fabian Guayacan

--- src/main/java/edu/softwaretesting2/dummymavenapp/vaccination_app/service/VacunacionService.java
+++ src/main/java/edu/softwaretesting2/dummymavenapp/vaccination_app/service/VacunacionService.java
@@ -5,9 +5,11 @@ import edu.softwaretesting2.dummymavenapp.vaccination_app.model.Paciente;
 import java.time.LocalDate;
 
 public class VacunacionService {
-    public void validarLaEdadDelPaciente(Paciente p) {
+    public boolean validarLaEdadDelPaciente(Paciente p) {
         LocalDate fechaLímite = LocalDate.now().minusYears(60);
+        boolean esPrioritario = p.getFechaDeNacimiento().isBefore(fechaLímite);
 
-        p.setPrioritario(p.getFechaDeNacimiento().isBefore(fechaLímite));
+        p.setPrioritario(esPrioritario);
+        return esPrioritario;
     }
 }
